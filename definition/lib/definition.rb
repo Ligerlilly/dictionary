635382ddac2cf3530f0507bb9a1e71e6d04f5133
Jason <quizathaderat@yahoo.com>
2015-08-14 09:40:47 -0700

    Add class method all to Definition

--- definition/lib/definition.rb
+++ definition/lib/definition.rb
@@ -1,6 +1,13 @@
 class Definition
   attr_accessor :definition
+  @@def_array = []
   def initialize(definition)
     @definition = definition
+    @id = @@def_array.length + 1
   end
+
+  def self.all
+    @@def_array 
+  end
+
 end
--- definition/spec/definition_spec.rb
+++ definition/spec/definition_spec.rb
@@ -6,8 +6,14 @@ describe Definition do
     @def = Definition.new("Fictional cartoon character")
   end
   describe '#definition' do
-    it 'does some stuff' do
+    it 'returns string object was initialized with' do
       expect(@def.definition).to eq "Fictional cartoon character"
+    end
+  end
+
+  describe '.all' do
+    it 'returns an empty array at first' do
+      expect(Definition.all).to eq []
     end
   end
 end
