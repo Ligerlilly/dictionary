2973700382d74d3ba1f207281cc5963ac1b5c7c8
Jason <quizathaderat@yahoo.com>
2015-08-14 12:48:28 -0700

    Pass add definition integration spec

--- app.rb
+++ app.rb
@@ -8,17 +8,33 @@ get('/') do
 	erb :index
 end
 
-get '/add_word' do
+get '/word_form' do
   erb :word_form
 end
 
-post '/add_word_form' do
+post '/add_word' do
 	@word = Word.new({ word: params['word'] })
+	@def = Definition.new
 	@word.save
+	@def.save
   redirect '/'
 end
 
 get '/word/:id/def' do
 	@word = Word.find(params['id'].to_i)
+	@def = Definition.find(params['id'].to_i)
   erb :defs
+end
+
+get '/word/:id/def_form' do
+	@word = Word.find(params["id"].to_i)
+  erb :def_form
+end
+
+post '/add_def' do
+	@def = Definition.find(params['word'].to_i)
+	@word = Word.find(params['word'].to_i)
+	@def.add_def(params['def'])
+
+	redirect "/word/#{@word.id}/def"
 end
--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -6,9 +6,22 @@ set(:show_exceptions, false)
 describe 'dictionary path', { type: :feature } do
   it 'takes user input and creates a word' do
     visit '/'
-    click_link 'Add Word'
+    first(:link, 'Add Word').click
     fill_in 'word', with: 'Homer'
     click_button 'Submit'
     expect(page).to have_content 'Homer'
+  end
+
+  it 'take user input and creates a definition for a word' do
+    visit '/'
+    first(:link, 'Add Word').click
+    fill_in 'word', with: 'Homer'
+    click_button 'Submit'
+    first(:link, 'Homer').click
+    click_link 'Add Definition'
+    fill_in 'def', with: 'Fictional cartoon character'
+    click_button 'Submit'
+    expect(page).to have_content 'Definitions'
+    expect(page).to have_content 'Fictional cartoon character'
   end
 end
